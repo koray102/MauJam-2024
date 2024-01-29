fceaff7e20e158df59e5741dd68625f08dae7129
koray102 <korayvol2@gmail.com>
2024-01-29 21:31:26 +0300

    deneme koray 1

---
+++ Assets/deneme.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deneme : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        koray
+        1
+        2
+        3
+        4
+        5
+    }
+}
